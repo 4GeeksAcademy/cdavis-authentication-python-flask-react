b9fb7fd992f0f7d017ddffbdf77cd43afa4a131c
cdavis2223 <cdavis2223@gmail.com>
2024-01-30 02:06:33 +0000

    First commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,9 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+#from app import bcrypt
+from flask_jwt_extended import create_access_token, jwt_required
+
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +23,38 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def user_create():
+    data = request.get_json()
+    email=data["email"]
+    password=data["password"]
+    user=User()
+    user.email=email
+    print(data)
+    user.password=password
+    user.is_active=True
+    db.session.add(user)
+    db.session.commit()
+    return "ok"
+
+@api.route('/login', models=['POST'])
+def user_login():
+    data=request.get_json()
+    user=User.query.filter_by(email=data['email']).first()
+    if user is None:
+        return jsonify({error: "Usuario no encontrado"}), 401
+    if user.password!=data["password"]
+        return jsonify({"error":"Clave inválida"}), 401
+    print(user)
+    payload={"email":user.email,"nivel": "Administrador"}
+    token=create_access_token(identity=user.id, additional_claims=payload)
+    return jsonify({"token":token})
+
+@api.route('/helloprotected', methods=['GET'])
+@jwt_required()
+def hello_protected():
+    id=get_jwt_identity()
+    payload=get_jwt()
+    return jsonify({"id":id, "rol": payload["nivel"]})
+    #data=request.get_json()
--- src/app.py
+++ src/app.py
@@ -5,11 +5,13 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+#from flask_bcrypt import Bcrypt
 
 # from models import Person
 
@@ -18,6 +20,11 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_APP_KEY")
+
+jwt=JWTManager(app)
+#bcrypt=Bcrypt(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
